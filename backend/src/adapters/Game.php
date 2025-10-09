2cb9f1aafe76cfe7d0d38b59c04b793a0962b8fc
Olivier Azeau <git@azeau.com>
2025-10-09 10:11:54 +0200

    adapt to framework changes

--- backend/src/adapters/Game.php
+++ backend/src/adapters/Game.php
@@ -55,16 +55,6 @@ class Game extends \Table
     }
 
     /**
-     * Returns the game name.
-     *
-     * IMPORTANT: Please do not modify.
-     */
-    protected function getGameName()
-    {
-        return "agileandco";
-    }
-
-    /**
      * This method is called only once, when a new game is launched. In this method, you must setup the game
      *  according to the game rules, so that the game is ready to be played.
      */
